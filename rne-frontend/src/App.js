041cce8ff844e3e1da37cb9d5ec093833eec20d3
Leonardo Zambrano <leonardoz.bta@gmail.com>
2024-11-07 23:49:22 -0500

    Update App.js

--- rne-frontend/src/App.js
+++ rne-frontend/src/App.js
@@ -6,20 +6,14 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
-import Modal from './components/Modal';
 import Header from "./components/Header";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
-
   return (
     <div>
       <Header />
       <Navbar />
-      <MainContent showModal={showModal} openModal={openModal} closeModal={closeModal} />
+      <MainContent />
       <Footer />
     </div>
   );
