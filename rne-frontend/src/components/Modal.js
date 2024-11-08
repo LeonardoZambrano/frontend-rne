13a239faeb0fc9f60c9a71968fbcd44397d08099
Leonardo Zambrano <leonardoz.bta@gmail.com>
2024-11-07 23:49:49 -0500

    Update Modal.js

--- rne-frontend/src/components/Modal.js
+++ rne-frontend/src/components/Modal.js
@@ -1,5 +1,7 @@
+import React from "react";
+
 // Componente Modal
-function Modal({ showModal, closeModal }) {
+function Modal({ showModal, closeModal, content }) {
     if (!showModal) return null;
 
     return (
@@ -7,20 +9,10 @@ function Modal({ showModal, closeModal }) {
             <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
                 <div className="modal-content">
                     <div className="modal-header header-bg-blue">
-                        <h2 className="modal-title">Términos y condiciones</h2>
-                        <button type="button" className="close" onClick={closeModal}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                        <h2 className="modal-title">{content.title}</h2>
                     </div>
                     <div className="modal-body">
-                        <h4>1. Del Servicio</h4>
-                        <p>
-                            El Registro de Números Excluidos - RNE - es un servicio que
-                            administra la CRC en el que se pueden inscribir los consumidores...
-                        </p>
-                        <h4>2. De Las Responsabilidades de la CRC</h4>
-                        <p>La Comisión de Regulación de Comunicaciones es la encargada...</p>
-                        {/* Agrega el contenido completo aquí */}
+                        {content.body}
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" onClick={closeModal}>
